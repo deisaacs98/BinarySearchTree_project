57ea45bf984ad556c9fa66fcb05d364e28ff2de9
Dan Isaacs <deisaacs98@gmail.com>
2021-02-05 11:47:11 -0600

    init

---
+++ BinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree<T>: IEnumerable where T: IComparable
+    {
+        private T[] items = new T[4];
+        private int count;
+        public BinarySearchTree()
+        {
+
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+    }
+}
